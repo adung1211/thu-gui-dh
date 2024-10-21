dca392f4b25725234765aa0b5f69b32e25128246
adungBee <anh.dung@beehexa.com>
2024-10-21 12:24:35 +0700

    update

--- src/mobileui/MobileUI.tsx
+++ src/mobileui/MobileUI.tsx
@@ -18,6 +18,11 @@ const MobileUI = () => (
             <img src={circlePath} style={{width: '30%', justifySelf: 'center', paddingTop: '10px'}}></img>
             <img src={thuguiPath} style={{width: '30%', justifySelf: 'start', alignSelf: 'end', paddingLeft: '20px'}}></img>
             <div className='m-custom-text-box'></div>
+            <div className='m-row2'>
+                <img src={bottomleftPath}  style={{width: '45%', position: 'absolute', bottom: '0'}}></img>
+                <img src={bottomrightPath}  style={{width: '45%', position: 'absolute', bottom: '0', right: '0'}}></img>
+                <img src={bottomlinePath}  style={{width: '100%', position: 'absolute', bottom: '0'}}></img>
+            </div>
         </div>
     </div>
 );
